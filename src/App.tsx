0c37806abb919bec72577f9c1c7716ff1d1a1998
Valentina Valverde <vvgreenville@gmail.com>
2023-12-23 16:53:35 -0500

    Moved buttons to component (instead of having that logic in App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import UserInput from "./components/UserInput";
-import { useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  const handleClick = async () => {
-    try {
-      setLoading(true);
-      console.log("this works");
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <>
       <h1>madlib</h1>
       <UserInput />
-      {/* <button onClick={handleClick}>button</button> */}
-      {loading && <p>Loading...</p>}
     </>
   );
 }
--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -2,21 +2,31 @@ import { FC, useState } from "react";
 
 const UserInput: FC = () => {
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      console.log("this works");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+      event?.preventDefault();
+      setInput("you've clicked me!");
+    }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    event?.preventDefault();
-    setInput("you've clicked me!");
   };
 
   return (
     <>
       <form>
         <input placeholder="user input" type="text" onChange={handleChange} />
-        <button onClick={handleSubmit}>click me!</button>
+        <button onClick={handleClick}>click me!</button>
+        {loading && <p>Loading...</p>}
         <p>{input}</p>
       </form>
     </>
